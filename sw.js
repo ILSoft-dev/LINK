e61dfd1d0b8bbcc4b5324b51367e2a8eb08d5a2a
ILSoft-dev <ispitonyourstats@gmail.com>
2026-09-23 14:16:15 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // либо загружается пользователем заново; сервис-воркер отвечает только за то,
 // чтобы САМО приложение открывалось без сети.
 
-const CACHE_VERSION = 'link-shell-v11';
+const CACHE_VERSION = 'link-shell-v12';
 const APP_SHELL = [
     './',
     './index.html',
